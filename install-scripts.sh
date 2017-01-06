416ea257d8c68bea457c483350801e0d1333b555
Neil Sharpe <nsharpe@gmail.com>
2017-01-06 11:17:10 -0800

    Create install-scripts.sh

---
+++ install-scripts.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+mkdir ~/scripts
+
+curl -K https://raw.githubusercontent.com/nsharpe/mac-dev-setup/scripts/git-rebase-master.sh >> ~/scripts/git-rebase-master.sh
+
+chmod 700 ~/scripts/git-rebase-master.sh
