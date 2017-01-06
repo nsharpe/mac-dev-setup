eb713f02e9a6a9e489359e74cc3f5ebac32a3b02
Neil Sharpe <nsharpe@gmail.com>
2017-01-06 11:28:07 -0800

    Update install-scripts.sh

--- install-scripts.sh
+++ install-scripts.sh
@@ -2,6 +2,6 @@
 
 mkdir ~/scripts
 
-curl "https://raw.githubusercontent.com/nsharpe/mac-dev-setup/master/scripts/git-rebase-master.sh" --output "~/scripts/git-rebase-master.sh"
+curl  -o ~/scripts/git-rebase-master.sh https://raw.githubusercontent.com/nsharpe/mac-dev-setup/master/scripts/git-rebase-master.sh
 
 chmod 700 ~/scripts/git-rebase-master.sh
