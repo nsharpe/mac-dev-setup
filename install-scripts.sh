6f6dd9d9e3d4ca06aaa297c8b0b3f3def6b2fc91
Neil Sharpe <nsharpe@gmail.com>
2017-01-23 11:26:30 -0800

    Include bash setup

--- install-scripts.sh
+++ install-scripts.sh
@@ -4,5 +4,7 @@ mkdir ~/scripts
 touch ~/scripts/non_persist.sh
 
 curl https://raw.githubusercontent.com/nsharpe/mac-dev-setup/master/scripts/git-rebase-master.sh > ~/scripts/git-rebase-master.sh
+mv ~/.bash_profile ~/.bash_profile.old
+curl https://raw.githubusercontent.com/nsharpe/mac-dev-setup/master/bash/.bash_profile > ~/.bash_profile
 
 chmod 700 ~/scripts/git-rebase-master.sh
