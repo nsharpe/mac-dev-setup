ad2b8678bd62f282d62abc22a81bd91e9b278edc
Neil Sharpe <nsharpe@gmail.com>
2020-02-24 16:01:48 -0800

    Update install-scripts.sh

--- install-scripts.sh
+++ install-scripts.sh
@@ -5,6 +5,6 @@ touch ~/scripts/non_persist.sh
 
 curl https://raw.githubusercontent.com/nsharpe/mac-dev-setup/master/scripts/git-rebase-master.sh > ~/scripts/git-rebase-master.sh
 mv ~/.bash_profile ~/.bash_profile.old
-curl https://raw.githubusercontent.com/nsharpe/mac-dev-setup/master/bash/.bash_profile > ~/.bash_profile
+curl https://raw.githubusercontent.com/nsharpe/mac-dev-setup/master/bash/.bash_profile > ~/.zshrc
 
 chmod 700 ~/scripts/git-rebase-master.sh
