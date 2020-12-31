8587cf403c6536add83e3f98d3aef23e9e125f91
Neil Sharpe <nsharpe@gmail.com>
2020-12-30 16:03:16 -0800

    lint

--- install-scripts.sh
+++ install-scripts.sh
@@ -4,6 +4,8 @@ mkdir ~/scripts
 touch ~/scripts/non_persist.sh
 
 curl https://raw.githubusercontent.com/nsharpe/mac-dev-setup/master/scripts/git-rebase-master.sh > ~/scripts/git-rebase-master.sh
+
+#Saves old .zshrc in case there is an issue.
 mv ~/.zshrc ~/.zshrc.old
 curl https://raw.githubusercontent.com/nsharpe/mac-dev-setup/master/bash/.bash_profile > ~/.zshrc
 
