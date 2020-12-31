1765c66d0071335f4848c0b94c457dcab267fb1e
Neil Sharpe <nsharpe@gmail.com>
2020-12-30 16:04:24 -0800

    Delete bash_setup.sh

--- bash/bash_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#  Example replace line that starts with alias
-# sed -i "/alias=/c\alias=newvalue" your_file_here
-
-sed -i "/export JAVA_HOME=/c\export JAVA_HOME=/"$(//usr//libexec//java_home -v 1.8)/"" ~/.bash_profile
